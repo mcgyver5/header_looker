8f9e0bbc3ef4960574b4bd467709e05059f26402
mcgyver5 <tim.e.mcguire@gmail.com>
2020-07-06 04:33:54 +0000

    remove commented lines

--- header_sucker.py
+++ header_sucker.py
@@ -56,8 +56,3 @@ for url in temp:
     hdrs = get_headers(req_headers, url)
     sleep(randint(1,2))
 
-
-# answer is a dictionary:
-# {'Content-Encoding': 'gzip', 'Cache-Control': 'public', 'Content-Type': 'text/html', 'Date': 'Thu Jul 02 2020 23:24:10 GMT-0500 (Central Daylight Time)', 'Server': 'Microsoft-IIS/7.5', 'Vary': 'Accept-Encoding', 'X-Frame-Options': 'SAMEORIGIN', 'X-Powered-By': 'PHP/5-4-2. ASP.NET', 'Content-Length': '0'}
-
-# 
